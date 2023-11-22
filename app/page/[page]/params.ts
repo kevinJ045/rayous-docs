6737bd3b7d127c186663c20e7f6cce6dae6aadbd
makano <makanobush@gmail.com>
2023-11-22 23:33:22 +0300

    params

--- app/page/[page]/params.ts
+++ app/page/[page]/params.ts
@@ -3,6 +3,6 @@ import { Sequence } from "@/app/constant/sequence";
 export function params(){
 	let seq: string[] = [];
 	Sequence.filter(t => typeof t == "string").forEach((t: string) => seq.push(t));
-	Sequence.filter(t => Array.isArray(t)).forEach((t: string[]) => seq.push(...t.slice(0, t.length)));
+	Sequence.filter(t => Array.isArray(t)).forEach((t: string[]) => seq.push(...t.slice(1, t.length)));
 	return seq.map(t => ({ page: t }));
 }
